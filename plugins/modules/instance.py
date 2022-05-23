d7468ef2c11732a656f5eaf5d463752591873163
LBGarber <lgarber@linode.com>
2022-05-23 11:17:02 -0400

    Add return formatting

--- plugins/modules/instance.py
+++ plugins/modules/instance.py
@@ -458,16 +458,130 @@ specdoc_meta = dict(
             description=['The instance description in JSON serialized form.'],
             docs_url='https://www.linode.com/docs/api/linode-instances/#linode-view__responses',
             type='dict',
+            sample=['''{
+  "alerts": {
+    "cpu": 180,
+    "io": 10000,
+    "network_in": 10,
+    "network_out": 10,
+    "transfer_quota": 80
+  },
+  "backups": {
+    "enabled": true,
+    "last_successful": "2018-01-01T00:01:01",
+    "schedule": {
+      "day": "Saturday",
+      "window": "W22"
+    }
+  },
+  "created": "2018-01-01T00:01:01",
+  "group": "Linode-Group",
+  "hypervisor": "kvm",
+  "id": 123,
+  "image": "linode/debian10",
+  "ipv4": [
+    "203.0.113.1",
+    "192.0.2.1"
+  ],
+  "ipv6": "c001:d00d::1337/128",
+  "label": "linode123",
+  "region": "us-east",
+  "specs": {
+    "disk": 81920,
+    "memory": 4096,
+    "transfer": 4000,
+    "vcpus": 2
+  },
+  "status": "running",
+  "tags": [
+    "example tag",
+    "another example"
+  ],
+  "type": "g6-standard-1",
+  "updated": "2018-01-01T00:01:01",
+  "watchdog_enabled": true
+}''']
         ),
         configs=dict(
             description=['A list of configs tied to this Linode Instance.'],
             docs_url='https://www.linode.com/docs/api/linode-instances/#configuration-profile-view__responses',
             type='list',
+            sample=['''[
+  {
+    "comments": "This is my main Config",
+    "devices": {
+      "sda": {
+        "disk_id": 124458,
+        "volume_id": null
+      },
+      "sdb": {
+        "disk_id": 124458,
+        "volume_id": null
+      },
+      "sdc": {
+        "disk_id": 124458,
+        "volume_id": null
+      },
+      "sdd": {
+        "disk_id": 124458,
+        "volume_id": null
+      },
+      "sde": {
+        "disk_id": 124458,
+        "volume_id": null
+      },
+      "sdf": {
+        "disk_id": 124458,
+        "volume_id": null
+      },
+      "sdg": {
+        "disk_id": 124458,
+        "volume_id": null
+      },
+      "sdh": {
+        "disk_id": 124458,
+        "volume_id": null
+      }
+    },
+    "helpers": {
+      "devtmpfs_automount": false,
+      "distro": true,
+      "modules_dep": true,
+      "network": true,
+      "updatedb_disabled": true
+    },
+    "id": 23456,
+    "interfaces": [
+      {
+        "ipam_address": "10.0.0.1/24",
+        "label": "example-interface",
+        "purpose": "vlan"
+      }
+    ],
+    "kernel": "linode/latest-64bit",
+    "label": "My Config",
+    "memory_limit": 2048,
+    "root_device": "/dev/sda",
+    "run_level": "default",
+    "virt_mode": "paravirt"
+  }
+]''']
         ),
         disks=dict(
             description=['A list of disks tied to this Linode Instance.'],
             docs_url='https://www.linode.com/docs/api/linode-instances/#disk-view__responses',
-            type='list'
+            type='list',
+            sample=['''[
+  {
+    "created": "2018-01-01T00:01:01",
+    "filesystem": "ext4",
+    "id": 25674,
+    "label": "Debian 9 Disk",
+    "size": 48640,
+    "status": "ready",
+    "updated": "2018-01-01T00:01:01"
+  }
+]''']
         )
     )
 )
