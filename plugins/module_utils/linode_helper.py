6106e4ec6bdb9ebb22d7c313b318ff929d3fcb44
LBGarber <lgarber@linode.com>
2022-08-24 12:00:45 -0400

    Add stackscript module

--- plugins/module_utils/linode_helper.py
+++ plugins/module_utils/linode_helper.py
@@ -83,39 +83,45 @@ def handle_updates(obj: linode_api4.Base, params: dict, mutable_fields: set, reg
     obj._api_get()
 
     # Update mutable values
-    should_update = False
     params = filter_null_values(params)
+
+    put_request = {}
 
     for key, new_value in params.items():
         if not hasattr(obj, key):
             continue
 
-        old_value = getattr(obj, key)
-
-        if type(old_value) in {
-            linode_api4.objects.linode.Type,
-            linode_api4.objects.linode.Region,
-            linode_api4.objects.linode.Image,
-            linode_api4.objects.lke.KubeVersion
-        }:
-            old_value = old_value.id
+        old_value = parse_linode_types(getattr(obj, key))
 
         if new_value != old_value:
             if key in mutable_fields:
-                setattr(obj, key, new_value)
+                put_request[key] = new_value
                 register_func('Updated {0}: "{1}" -> "{2}"'.
                                      format(key, old_value, new_value))
 
-                should_update = True
                 continue
 
             raise RuntimeError(
                 'failed to update {} -> {}: {} is a non-updatable'
                 ' field'.format(old_value, new_value, key))
 
-    if should_update:
-        obj.save()
+    if len(put_request.keys()) > 0:
+        obj._client.put(type(obj).api_endpoint, model=obj, data=put_request)
 
+
+def parse_linode_types(v: any) -> any:
+    if isinstance(v, list):
+        return [parse_linode_types(elem) for elem in v]
+
+    if type(v) in {
+        linode_api4.objects.linode.Type,
+        linode_api4.objects.linode.Region,
+        linode_api4.objects.linode.Image,
+        linode_api4.objects.lke.KubeVersion
+    }:
+        return v.id
+
+    return v
 
 def jsonify_node_pool(pool: LKENodePool) -> Dict[str, Any]:
     """Converts an LKENodePool into a JSON-compatible dict"""
---
+++ plugins/modules/stackscript.py
@@ -0,0 +1,177 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""This module contains all of the functionality for Linode StackScripts."""
+
+from __future__ import absolute_import, division, print_function
+
+# pylint: disable=unused-import
+import copy
+from typing import Optional, cast, Any, Set
+
+import polling
+from linode_api4 import StackScript
+
+from ansible_collections.linode.cloud.plugins.module_utils.linode_common import LinodeModuleBase
+
+from ansible_collections.linode.cloud.plugins.module_utils.linode_docs import global_authors, \
+    global_requirements
+
+from ansible_collections.linode.cloud.plugins.module_utils.linode_helper import handle_updates, filter_null_values
+
+import ansible_collections.linode.cloud.plugins.module_utils.doc_fragments.stackscript as docs
+
+SPEC = dict(
+    label=dict(
+        type='str',
+        required=True,
+        description='This StackScript\'s unique label.'),
+    state=dict(
+        type='str',
+        choices=['present', 'absent'],
+        required=True,
+        description='The state of this StackScript.',
+    ),
+
+    description=dict(
+        type='str',
+        description='A description for the StackScript.',
+    ),
+    images=dict(
+        type='list',
+        elements='str',
+        description='Images that can be deployed using this StackScript.',
+    ),
+    is_public=dict(
+        type='bool',
+        description='This determines whether other users can use your StackScript.',
+    ),
+    rev_note=dict(
+        type='str',
+        description='This field allows you to add notes for the set of revisions made to this StackScript.',
+    ),
+    script=dict(
+        type='str',
+        description='The script to execute when provisioning a new Linode with this StackScript.'
+    )
+)
+
+specdoc_meta = dict(
+    description=[
+        'Manage a Linode StackScript.'
+    ],
+    requirements=global_requirements,
+    author=global_authors,
+    spec=SPEC,
+    examples=docs.specdoc_examples,
+    return_values=dict(
+        stackscript=dict(
+            description='The StackScript in JSON serialized form.',
+            docs_url='https://www.linode.com/docs/api/stackscripts/'
+                     '#stackscript-create__response-samples',
+            type='dict',
+            sample=docs.result_stackscript_samples
+        )
+    )
+)
+
+MUTABLE_FIELDS = {
+    'description',
+    'images',
+    'is_public',
+    'rev_note',
+    'script'
+}
+
+
+class Module(LinodeModuleBase):
+    """Module for creating and destroying Linode StackScripts"""
+
+    def __init__(self) -> None:
+        self.module_arg_spec = SPEC
+        self.required_one_of = ['state', 'label']
+        self.results = dict(
+            changed=False,
+            actions=[],
+            stackscript=None,
+        )
+
+        super().__init__(module_arg_spec=self.module_arg_spec,
+                         required_one_of=self.required_one_of,
+                         required_if=[('state', 'present', ('images', 'script'))])
+
+    def _get_stackscript_by_label(self, label: str) -> Optional[StackScript]:
+        try:
+            return self.client.linode.stackscripts(StackScript.label == label)[0]
+        except IndexError:
+            return None
+        except Exception as exception:
+            return self.fail(msg='failed to get stackscript {0}: {1}'.format(label, exception))
+
+    def _create_stackscript(self) -> Optional[StackScript]:
+        params = copy.deepcopy(self.module.params)
+        label = params.pop('label')
+        script = params.pop('script')
+        images = params.pop('images')
+
+        try:
+            return self.client.linode.stackscript_create(label, script, images, **params)
+        except Exception as exception:
+            return self.fail(msg='failed to create stackscript: {0}'.format(exception))
+
+    def _update_stackscript(self, stackscript: StackScript) -> None:
+        stackscript._api_get()
+
+        params = filter_null_values(self.module.params)
+
+        handle_updates(stackscript, params, MUTABLE_FIELDS, self.register_action)
+
+    def _handle_present(self) -> None:
+        params = self.module.params
+
+        label = params.get('label')
+
+        stackscript = self._get_stackscript_by_label(label)
+
+        # Create the stackscript if it does not already exist
+        if stackscript is None:
+            stackscript = self._create_stackscript()
+            self.register_action('Created stackscript {0}'.format(label))
+
+        self._update_stackscript(stackscript)
+
+        # Force lazy-loading
+        stackscript._api_get()
+
+        self.results['stackscript'] = stackscript._raw_json
+
+    def _handle_absent(self) -> None:
+        label: str = self.module.params.get('label')
+
+        stackscript = self._get_stackscript_by_label(label)
+
+        if stackscript is not None:
+            self.results['stackscript'] = stackscript._raw_json
+            stackscript.delete()
+            self.register_action('Deleted stackscript {0}'.format(label))
+
+    def exec_module(self, **kwargs: Any) -> Optional[dict]:
+        """Entrypoint for StackScript module"""
+        state = kwargs.get('state')
+
+        if state == 'absent':
+            self._handle_absent()
+            return self.results
+
+        self._handle_present()
+
+        return self.results
+
+
+def main() -> None:
+    """Constructs and calls the StackScript module"""
+    Module()
+
+
+if __name__ == '__main__':
+    main()
