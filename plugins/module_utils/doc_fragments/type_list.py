171d07ec00053fe8dc3c1ba4cb9c4568105ce019
Jacob Riddle <87780794+jriddle-linode@users.noreply.github.com>
2022-11-28 11:28:25 -0500

    adding types list module (#240)
    ## 📝 Description
    
    Allows the listing of Linode Instance Types
    https://www.linode.com/docs/api/linode-types/#types-list
    
    ## ✔️ How to Test
    
    **What are the steps to reproduce the issue or verify the changes?**
    
    ```bash
    make TEST_ARGS='-v type_list' test
    ```
    ```yaml
    - linode.cloud.type_list:
         filter:
           - name: label
              values: Nanode 1GB
    ```

---
+++ plugins/module_utils/doc_fragments/type_list.py
@@ -0,0 +1,38 @@
+"""Documentation fragments for the type_list module"""
+
+specdoc_examples = ['''
+- name: List all of the Linode Instance Types
+  linode.cloud.type_list: {}''', '''
+- name: List a Linode Instance Type named Nanode 1GB
+  linode.cloud.type_list:
+    filter:
+      - name: label
+        values: Nanode 1GB
+''']
+
+result_type_samples = ['''[
+    {
+        "addons": {
+            "backups": {
+                "price": {
+                    "hourly": 0.008,
+                    "monthly": 5
+                }
+            }
+        },
+        "class": "standard",
+        "disk": 81920,
+        "gpus": 0,
+        "id": "g6-standard-2",
+        "label": "Linode 4GB",
+        "memory": 4096,
+        "network_out": 1000,
+        "price": {
+            "hourly": 0.03,
+            "monthly": 20
+        },
+        "successor": null,
+        "transfer": 4000,
+        "vcpus": 2
+    }
+]''']
