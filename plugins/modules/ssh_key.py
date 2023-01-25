a69bc6b307e5baf46ee05e1841ccf11572dd3bf7
zliang-akamai <121905282+zliang-akamai@users.noreply.github.com>
2023-01-25 11:38:18 -0600

    Ansible SSH Key Module (#269)
    * Implement ssh key module
    
    * Change var name to avoid duplicated name with other module's doc
    
    * Add docs
    
    * Fix
    
    * Remove white spaces
    
    Co-authored-by: Lena Garber <114949949+lgarber-akamai@users.noreply.github.com>

---
+++ plugins/modules/ssh_key.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""This module contains all of the functionality for Linode SSH keys."""
+
+from __future__ import absolute_import, division, print_function
+
+from typing import Optional, Any
+
+from linode_api4 import SSHKey
+
+from ansible_collections.linode.cloud.plugins.module_utils.linode_common import LinodeModuleBase
+
+from ansible_collections.linode.cloud.plugins.module_utils.linode_docs import global_authors, \
+    global_requirements
+
+from ansible_collections.linode.cloud.plugins.module_utils.linode_helper import \
+    handle_updates
+
+import ansible_collections.linode.cloud.plugins.module_utils.doc_fragments.ssh_key as docs
+
+ssh_key_spec = dict(
+    label=dict(
+        type='str',
+        required=True,
+        description='This SSH key\'s unique label.'),
+    state=dict(
+        type='str',
+        choices=['present', 'absent'],
+        required=True,
+        description='The state of this SSH key.',
+    ),
+    ssh_key=dict(
+        type='str',
+        editable=True,
+        description='The SSH public key value.'
+    )
+)
+
+specdoc_meta = dict(
+    description=[
+        'Manage a Linode SSH key.'
+    ],
+    requirements=global_requirements,
+    author=global_authors,
+    spec=ssh_key_spec,
+    examples=docs.specdoc_examples,
+    return_values=dict(
+        ssh_key=dict(
+            description='The created SSH key in JSON serialized form.',
+            docs_url='https://www.linode.com/docs/api/profile/'
+                     '#ssh-key-add__response-samples',
+            type='dict',
+            sample=docs.result_ssh_key_samples
+        )
+    )
+)
+
+MUTABLE_FIELDS = {'label'}
+
+class SSHKeyModule(LinodeModuleBase):
+    """Module for creating and destroying Linode SSH keys"""
+
+    def __init__(self) -> None:
+        self.module_arg_spec = ssh_key_spec
+        self.results = dict(
+            changed=False,
+            actions=[],
+            ssh_key=None,
+        )
+        super().__init__(module_arg_spec=self.module_arg_spec,
+                         required_if=[['state', 'present', ['ssh_key']]])
+
+    def _create_ssh_key(self) -> Optional[SSHKey]:
+        params = self.module.params
+        try:
+            return self.client.profile.ssh_key_upload(
+                params.get("ssh_key"),
+                params.get("label")
+            )
+        except Exception as exception:
+            return self.fail(msg='failed to create SSH key: {0}'.format(exception))
+
+    def _get_ssh_key_by_label(self, label: str) -> Optional[SSHKey]:
+        try:
+            return self.client.profile.ssh_keys(SSHKey.label == label)[0]
+        except IndexError:
+            return None
+        except Exception as exception:
+            return self.fail(
+                msg=f"failed to get SSH key {label}: {exception}"
+            )
+
+    def _update_ssh_key(self, ssh_key: SSHKey) -> None:
+        ssh_key._api_get()
+
+        handle_updates(ssh_key, self.module.params, MUTABLE_FIELDS, self.register_action)
+
+    def _handle_present(self) -> None:
+        params = self.module.params
+
+        label = params.get('label')
+
+        ssh_key = self._get_ssh_key_by_label(label)
+
+        # Create the ssh_key if it does not already exist
+        if ssh_key is None:
+            ssh_key = self._create_ssh_key()
+            self.register_action(f'Created SSH key {label}')
+
+        self._update_ssh_key(ssh_key)
+
+        # Force lazy-loading
+        ssh_key._api_get()
+
+        self.results['ssh_key'] = ssh_key._raw_json
+
+    def _handle_absent(self) -> None:
+        label: str = self.module.params.get('label')
+
+        ssh_key = self._get_ssh_key_by_label(label)
+
+        if ssh_key is not None:
+            self.results['ssh_key'] = ssh_key._raw_json
+            ssh_key.delete()
+            self.register_action('Deleted SSH key {0}'.format(label))
+
+    def exec_module(self, **kwargs: Any) -> Optional[dict]:
+        """Entrypoint for SSH key module"""
+        state = kwargs.get('state')
+
+        if state == 'absent':
+            self._handle_absent()
+            return self.results
+
+        self._handle_present()
+
+        return self.results
+
+
+def main() -> None:
+    """Constructs and calls the SSH key module"""
+    SSHKeyModule()
+
+
+if __name__ == '__main__':
+    main()
