f6d216eafc0b0ce44a526609813825189890f6a6
ezilber-akamai <ezilber@akamai.com>
2023-02-23 13:37:23 -0500

    Add instance_type_list module

---
+++ plugins/module_utils/doc_fragments/instance_type_list.py
@@ -0,0 +1,37 @@
+"""Documentation fragments for the instance_type_list module"""
+
+specdoc_examples = ['''
+- name: List all of the Linode instance types
+  linode.cloud.instance_type_list: {}''', '''
+- name: Resolve all Linode instance types
+  linode.cloud.instance_type_list:
+    filter:
+      - name: class
+        values: nanode''']
+
+result_images_samples = ['''[
+   {
+      "addons": {
+        "backups": {
+          "price": {
+            "hourly": 0.008,
+            "monthly": 5
+          }
+        }
+      },
+      "class": "standard",
+      "disk": 81920,
+      "gpus": 0,
+      "id": "g6-standard-2",
+      "label": "Linode 4GB",
+      "memory": 4096,
+      "network_out": 1000,
+      "price": {
+        "hourly": 0.03,
+        "monthly": 20
+      },
+      "successor": null,
+      "transfer": 4000,
+      "vcpus": 2
+    }
+]''']
