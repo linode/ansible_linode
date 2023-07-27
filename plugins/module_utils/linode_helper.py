6bec0584eae664dad177a376d4474d10eadf3172
Lena Garber <lgarber@akamai.com>
2023-07-27 16:24:34 -0400

    Refactor get_all_paginated

--- plugins/module_utils/linode_helper.py
+++ plugins/module_utils/linode_helper.py
@@ -1,5 +1,6 @@
 """This module contains helper functions for various Linode modules."""
 import math
+import sys
 from typing import Any, Callable, Dict, List, Optional, Set, Tuple, cast
 
 import linode_api4
@@ -251,13 +252,17 @@ def get_all_paginated(
     result = []
     current_page = 1
     page_size = 100
-    num_pages = -1
+    num_pages = 1
+
+    # Don't cap the number of results if
+    # num_results is not defined.
+    num_results = sys.maxsize if num_results is None else num_results
 
     if num_results is not None and num_results < page_size:
         # Clamp the page size
         page_size = max(min(num_results, 100), 25)
 
-    while current_page <= num_pages or num_pages == -1:
+    while current_page <= num_pages and len(result) < num_results:
         response = client.get(
             endpoint + "?page={}&page_size={}".format(current_page, page_size),
             filters=filters,
@@ -266,18 +271,11 @@ def get_all_paginated(
         if "data" not in response or "page" not in response:
             raise Exception("Invalid list response")
 
-        # There are no pages for this request.
-        # Break early to avoid division by zero error.
-        if response["pages"] == 0:
+        result.extend(response["data"])
+
+        if len(result) >= num_results:
             break
 
-        if num_pages == -1:
-            if num_results is not None:
-                num_pages = math.floor(num_results / response["pages"])
-            else:
-                num_pages = math.floor(response["results"] / response["pages"])
-
-        result.extend(response["data"])
         current_page += 1
 
     if num_results is not None:
