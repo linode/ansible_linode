824a7d0cfb9e5794c585b82f8373bdd66d4e15e9
Lena Garber <lgarber@akamai.com>
2024-03-29 15:58:18 -0400

    Translate stackscript_data to disk kwargs

--- plugins/modules/instance.py
+++ plugins/modules/instance.py
@@ -816,10 +816,9 @@ class LinodeInstance(LinodeModuleBase):
                 self.client, params.pop("stackscript_id")
             )
 
-        # Workaround for an edge case where a StackScript has no UDFs
-        # and the API rejects an empty list for stackscript_data
-        if len(params.get("stackscript_data") or {}) < 1:
-            params.pop("stackscript_data")
+        # StackScript data is expected to be specified as kwargs
+        if params.get("stackscript_data") is not None:
+            params.update(params.pop("stackscript_data"))
 
         # Workaround for race condition on implicit events
         # See: TPT-2738
