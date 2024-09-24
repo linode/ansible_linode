16fb64a4c3ea9db977439089d81bac0940e0e6ec
Ye Chen <yechen@akamai.com>
2024-09-24 14:52:17 -0400

    fix lint

--- plugins/module_utils/linode_common_info.py
+++ plugins/module_utils/linode_common_info.py
@@ -110,10 +110,11 @@ class InfoModuleResult:
     ] = None
 
 
-class InfoModule(LinodeModuleBase):  # pylint: disable=too-many-positional-arguments
+class InfoModule(LinodeModuleBase):
     """A common module for listing API resources given a set of filters."""
 
     def __init__(
+        # pylint: disable=too-many-positional-arguments
         self,
         primary_result: InfoModuleResult,
         secondary_results: List[InfoModuleResult] = None,
