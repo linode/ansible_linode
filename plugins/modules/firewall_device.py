10ae719fe7019afe801c7b12a5ca6959b43aacc2
Zhiwei Liang <zliang@akamai.com>
2025-06-05 17:01:56 -0400

    Fix doc for entity_type

--- plugins/modules/firewall_device.py
+++ plugins/modules/firewall_device.py
@@ -39,7 +39,7 @@ MODULE_SPEC = {
         type=FieldType.string,
         required=True,
         description=[
-            "The type of Linode Entity. Currently only supports linode and nodebalancer."
+            "The type of Linode Entity. Currently only supports linode, nodebalancer, and interface."
         ],
         choices=["linode", "nodebalancer", "interface"],
     ),
