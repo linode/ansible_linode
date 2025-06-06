98a93ad50d4206f742237f1f114e064e5e8222d2
Zhiwei Liang <zliang@akamai.com>
2025-06-06 00:58:14 -0400

    Implement firewall template modules

--- plugins/modules/firewall_settings_info.py
+++ plugins/modules/firewall_settings_info.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-"""This module contains all of the functionality for Linode Firewalls Settings."""
+"""This module allows users to retrieve information about Linode Firewalls Settings for the current account."""
 
 from __future__ import absolute_import, division, print_function
 
