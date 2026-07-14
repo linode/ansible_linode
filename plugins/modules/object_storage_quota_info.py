cb34fe1ef19d950b836f37ad833627b463572c49
Zhiwei Liang <zliang@akamai.com>
2026-07-13 23:13:53 -0400

    Only set obj quota usage when has_usage is true

--- plugins/modules/object_storage_quota_info.py
+++ plugins/modules/object_storage_quota_info.py
@@ -5,16 +5,36 @@
 
 from __future__ import absolute_import, division, print_function
 
+from typing import Any
+
 from ansible_collections.linode.cloud.plugins.module_utils.doc_fragments import (
     object_storage_quota_info as docs,
 )
 from ansible_collections.linode.cloud.plugins.module_utils.linode_common_info import (
     InfoModule,
     InfoModuleAttr,
+    InfoModuleParam,
     InfoModuleResult,
 )
 from ansible_specdoc.objects import FieldType
-from linode_api4 import ObjectStorageQuota
+from linode_api4 import LinodeClient, ObjectStorageQuota
+
+
+def get_quota_usage(
+    client: LinodeClient,
+    object_storage_quota: dict[str, Any],
+    params: list[InfoModuleParam],
+) -> dict[str, Any] | None:
+    """Return quota usage details for a quota when usage is available."""
+    if not object_storage_quota["has_usage"]:
+        return None
+
+    return (
+        ObjectStorageQuota(client, object_storage_quota["quota_id"])
+        .usage()
+        .dict
+    )
+
 
 module = InfoModule(
     examples=docs.specdoc_examples,
@@ -33,11 +53,7 @@ module = InfoModule(
             docs_url="https://techdocs.akamai.com/linode-api/reference"
             "/get-object-storage-quota-usage",
             samples=docs.result_object_storage_quota_usage_samples,
-            get=lambda client, object_storage_quota, params: ObjectStorageQuota(
-                client, object_storage_quota["quota_id"]
-            )
-            .usage()
-            .dict,
+            get=get_quota_usage,
         ),
     ],
     attributes=[
