e7b192d79a0d58d560b02cb402929c787919c570
Erik Zilber <ezilber@akamai.com>
2026-07-15 13:56:12 -0400

    TPT-4474: Configurable VPC IPv4 Prefixes (#827)
    * TPT-4478: Added support for Configurable VPC IPv4 Prefixes (#820)
    
    * Added support for Configurable VPC IPv4 Prefixes
    
    * Point to latest Python SDK release
    
    * Address PR comments
    
    * Fixed docs

---
+++ plugins/modules/vpc_default_ranges_info.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""Implementation for linode.cloud.vpc_default_ranges_info module."""
+
+from __future__ import absolute_import, division, print_function
+
+from ansible_collections.linode.cloud.plugins.module_utils.doc_fragments import (
+    vpc_default_ranges as docs,
+)
+from ansible_collections.linode.cloud.plugins.module_utils.linode_common_info import (
+    InfoModule,
+    InfoModuleResult,
+)
+from ansible_specdoc.objects import FieldType
+
+module = InfoModule(
+    examples=docs.specdoc_examples,
+    primary_result=InfoModuleResult(
+        display_name="VPC Default Ranges",
+        field_name="vpc_default_ranges",
+        field_type=FieldType.dict,
+        docs_url="https://techdocs.akamai.com/linode-api/reference/get-vpcs-default-ranges",
+        samples=docs.result_samples,
+        get=lambda client, params: client.vpcs.default_ranges()._serialize(),
+    ),
+)
+
+SPECDOC_META = module.spec
+
+DOCUMENTATION = r"""
+"""
+EXAMPLES = r"""
+"""
+RETURN = r"""
+"""
+
+if __name__ == "__main__":
+    module.run()
