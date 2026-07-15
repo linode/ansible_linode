dae4779cb80cb3a94f8db48920fc254011e29ea9
Erik Zilber <ezilber@akamai.com>
2026-07-15 11:59:50 -0400

    TPT-4478: Added support for Configurable VPC IPv4 Prefixes (#820)
    * Added support for Configurable VPC IPv4 Prefixes
    
    * Point to latest Python SDK release
    
    * Address PR comments

--- plugins/module_utils/doc_fragments/vpc_list.py
+++ plugins/module_utils/doc_fragments/vpc_list.py
@@ -19,6 +19,11 @@ result_vpc_samples = ['''[
                 "range": "2001:db8:acad:0::/52"
             }
         ],
+        "ipv6": [
+            {
+                "range": "10.0.0.1/16"
+            }
+        ],
         "label": "my-vpc",
         "region": "us-east",
         "subnets": [],
